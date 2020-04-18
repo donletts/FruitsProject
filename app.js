fdf08d08e58813054a58acbb8ce54de375b6065e
Don Letts <don.letts@gmail.com>
2020-04-18 16:30:05 -0400

    switching out native mongo calls to mongoose ORM for inserting objects

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // this is about the same as "use fruitsDB" on the command line
 
-const MongoClient = require("mongodb").MongoClient;
-const assert = require("assert");
+const mongoose = require("mongoose");
 
 // Connection URL
 const url = 'mongodb://localhost:27017';
@@ -9,23 +8,72 @@ const url = 'mongodb://localhost:27017';
 // Database Name
 const dbName = 'fruitsDB';
 
-// Create a new MongoClient
-const client = new MongoClient(url,  { useUnifiedTopology: true } );
+// Create a new mongoose
+const client = new mongoose.connect(
+    url + "/" + dbName,
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    } );
 
-// Use connect method to connect to the Server
-client.connect(function(err){
-   assert.equal(null, err);
-    console.log ("Connected Successfully to server");
+const fruitSchema = new mongoose.Schema({
+   name: String,
+   rating: Number,
+   review: String
+});
 
-    const db = client.db(dbName);
-    insertFruits(db, function (result) {
-        console.log ("we did a thing");
-        console.log (result);
-        client.close();
-    })
-    findFruits(db, function (result) {
-        console.log (result);
-    });
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const apple = new Fruit( {
+    name: "Apple",
+    rating: 7,
+    review: "Pretty solid as a fruit"
+});
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+   name: "John",
+   age: 65
+});
+
+// person.save();
+
+const kiwi = new Fruit({
+   name: "Kiwi",
+   rating: 3,
+   review: "kind of a hairy fruit"
+});
+
+const orange = new Fruit({
+    name: "Orange",
+    rating: 4,
+    review: "Too sour for me"
+});
+
+const banana = new Fruit({
+   name: "banana",
+   rating: 3,
+   review: "weird texture"
+});
+
+Fruit.insertMany([
+    kiwi,
+    orange,
+    banana,
+    apple
+], function (err) {
+    if(err){
+        console.log (err);
+    }
+    else{
+        console.log ("successfully saved all the fruits");
+    }
 });
 
 const insertFruits = function (db, callback) {
